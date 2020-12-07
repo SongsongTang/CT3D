4d6661513e3f6a2d2c75b5a15390ebe0dd439136
tss1375716572 <tss1375716572@163.com>
2020-12-07 17:03:06 +0800

    ccbr finished version

--- main.py
+++ main.py
@@ -1,95 +1,130 @@
 import numpy as np
+import mat73
 import time
 from scipy.signal import convolve
-import mat73
 import matplotlib.pyplot as plt
 import scipy.io as sio
 
 
-class FDK(object):
-    def __init__(self, fname):
-
-        stime = time.time()
-
-        raw_data = np.fromfile(fname, np.float32)
-        self.data = raw_data.reshape(360, 256, 256) # reshape projected data as (beta, a, b)
+class CCBR(object):
+    """
+        FDK method of Circular Cone-Beam Reconstruction
+    """
+    def __init__(self):
         
-        self.kn = np.linspace(-255, 255, 511)
-        self.R = 40
-        self.l = 20
-        self.n = 256
-        self.T = self.l / self.n
-        self.b = np.linspace((self.l - self.T) / 2, -(self.l - self.T) / 2, self.n)[np.newaxis, np.newaxis, :]
-        self.a = np.linspace((self.l - self.T) / 2, -(self.l - self.T) / 2, self.n)[np.newaxis, :, np.newaxis]
-        pre_weighting_factor = self.R / np.sqrt(self.R ** 2 + self.a ** 2 + self.b **2)
-        self.pw_data = pre_weighting_factor * self.data
-        # convolve_kernel = 2 / ((np.pi * self.T) ** 2 * (4 * self.kn ** 2 - 1))[np.newaxis, :, np.newaxis]
-        convolve_kernel = self.rl_filter(self.kn, self.T)
-        self.f_data = convolve(self.pw_data, convolve_kernel, mode='same')
-        # print(self.f_data[0, :, 127])
-
-        self.x = np.linspace(-(self.l - self.T) / 2, (self.l - self.T) / 2, self.n)[np.newaxis, np.newaxis, :]
-        self.y = np.linspace(-(self.l - self.T) / 2, (self.l - self.T) / 2, self.n)[np.newaxis, :, np.newaxis]
-        self.z = np.linspace(-(self.l - self.T) / 2, (self.l - self.T) / 2, self.n)[:, np.newaxis, np.newaxis]
-        self.beta = np.arange(0, 2*np.pi, np.pi/180)
-        self.f_fdk = np.zeros((256, 256, 256))
-        for beta in self.beta:
-            U = self.R + self.x * np.cos(beta) + self.y * np.sin(beta)
-            a = self.R / U * (-self.x * np.sin(beta) + self.y * np.cos(beta))
-            b = self.R * self.z / U
-            a_around = -(np.around((a - self.T / 2) / self.T) * self.T + self.T / 2)
-            b_around = -(np.around((b - self.T / 2) / self.T) * self.T + self.T / 2)
-            # print(b, "*"*50, b_around)
-            a_around[a_around > ((self.l - self.T) / 2)] = (self.l - self.T) / 2
-            a_around[a_around < ((-self.l + self.T) / 2)] = (-self.l + self.T) / 2
-            b_around[b_around > ((self.l - self.T) / 2)] = (self.l - self.T) / 2
-            b_around[b_around < ((-self.l + self.T) / 2)] = (-self.l + self.T) / 2
-            # print(a_around, '\n', b_around)
-            a_index = ((a_around + (self.l - self.T) / 2) / self.T).astype(int)
-            b_index = ((b_around + (self.l - self.T) / 2) / self.T).astype(int)
-            print(int(np.around(beta*180/np.pi)), end='\t')
-            f = self.R**2 / U**2 * self.f_data[int(np.around(beta*180/np.pi)), a_index, b_index]
-            # print(f[127, 95, :])
+        stime = time.time()
+        
+        dir = "./"
+        self.fname = dir + "data/Circular CBCT_flat_panel_detector.prj"
+        self.R = 40     # the distance between source and origin
+        self.l = 20     # the total length of virtual detectors or Shepplogan
+        self.n = 256    # the number of detector or pixel per row
+        self.T = self.l / self.n    # the length of each detector or each pixel
+        # the half length of virtual detectors or Shepplogan
+        self.hl = (self.l - self.T) / 2
+        self.kn = np.linspace(-255, 255, 511)   # the sample of convolve kernel
+        raw_data = np.fromfile(self.fname, np.float32)
+        # reshape the data as (beta, b, a)
+        self.data = raw_data.reshape(360, 256, 256)
+        self.pw_data = self.pre_weight()    # pre-weight the data
+        # define the convolve kernel
+        self.conv_kn = self.rl_filter()[np.newaxis, np.newaxis, :]
+        self.conv_data = convolve(self.pw_data, self.conv_kn, mode='same')
+ 
+        # define x, y, z, beta array
+        self.x = np.linspace(-self.hl, self.hl, self.n)[np.newaxis, \
+                                                        np.newaxis, :]
+        self.y = np.linspace(-self.hl, self.hl, self.n)[:, np.newaxis, \
+                                                        np.newaxis]
+        self.z = np.linspace(self.hl, -self.hl, self.n)[np.newaxis, :, \
+                                                        np.newaxis]
+        beta_array = np.arange(0, 2 * np.pi, np.pi / 180)
+        # initial f_FDK(y, z, x)
+        self.f_fdk = np.zeros((self.n, self.n, self.n))
+        # integrate f_fdk by beta
+        for self.beta in beta_array:
+            U, a, b, beta = self.get_Uabbeta_index()
+            f = self.R ** 2 / U ** 2 * self.conv_data[beta, b, a] * np.pi / 180
             self.f_fdk += f
-        mdic = {"fdk_shepplogan": self.f_fdk}
-        sio.savemat("./data/fdk_shepplogan.mat", mdic)
+            print("\r Progress: " + str(np.around((self.beta+np.pi/180) / \
+                                                  np.pi*50, 1)) + "%", end='')
+        # transpose to f(z, y, x) corresponding to Shepplogan
+        self.f_fdk = (1 / 2) * self.f_fdk.transpose((1, 0, 2))
+        # save as .mat file
+        mdic = {"ccbr_fdk_shepplogan": self.f_fdk}
+        sio.savemat(dir + "data/ccbr_fdk_shepplogan.mat", mdic)
+ 
         etime = time.time()
-        print(np.around(etime - stime))
-
-    def rl_filter(self, n, T):
-        """
-            R-L滤波器
-            n为采样点数
-            T为采样间隔
-        """
-        f = np.zeros(np.shape(n))
+        print("\n Time: " + str(np.around(etime - stime, 1)) + "s!")
+ 
+    def pre_weight(self):
+        # define a, b array
+        a_array = np.linspace(-self.hl, self.hl, self.n)[np.newaxis, \
+                                                         np.newaxis, :]
+        b_array = np.linspace(-self.hl, self.hl, self.n)[np.newaxis, :, \
+                                                         np.newaxis]
+        pre_weighting_factor = self.R / np.sqrt(self.R ** 2 + a_array ** 2 + \
+                                                b_array ** 2)
+        pre_weighted_data = pre_weighting_factor * self.data
+        return pre_weighted_data
+    
+    def rl_filter(self):
+        # define R-L filter
+        f = np.zeros(np.shape(self.kn))
         j = 0
-        for i in n:
+        for i in self.kn:
             i = int(i)
             if i == 0:
-                f[j] = 1/(2*T)**2
+                f[j] = 1/(2*self.T)**2
             elif i%2:
-                f[j] = -1/(i*np.pi*T)**2
+                f[j] = -1/(i*np.pi*self.T)**2
             else:
                 f[j] = 0
             j += 1
-        return f[np.newaxis, :, np.newaxis]
+        return f
+    
+    def get_Uabbeta_index(self):
+        """Round to nearest"""
+        # calculate U
+        U = self.R + self.x * np.cos(self.beta) + self.y * np.sin(self.beta)
+        # calculate a, b
+        a = self.R / U * (-self.x * np.sin(self.beta) + self.y * \
+                          np.cos(self.beta))
+        b = self.R * self.z / U
+        a[a > self.hl] = self.hl
+        a[a < -self.hl] = -self.hl
+        b[b > self.hl] = self.hl
+        b[b < -self.hl] = -self.hl
+        a_around = (np.around((a - self.T / 2) / self.T) * self.T + self.T / 2)
+        b_around = (np.around((b - self.T / 2) / self.T) * self.T + self.T / 2)
+        a_index = ((self.hl + a_around) / self.T).astype(int)
+        b_index = ((self.hl + b_around) / self.T).astype(int)
+        beta_index = int(np.around(self.beta*180/np.pi))
+        return U, a_index, b_index, beta_index
 
 
 class Analysis(object):
     def __init__(self):
-        original = mat73.loadmat('data/Shepplogan.mat')['Shepplogan']
-        fdk_data = sio.loadmat('data/fdk_shepplogan.mat')['fdk_shepplogan']
-        plt.figure("ori")
+        dir = "./"
+        original = mat73.loadmat(dir + 'data/Shepplogan.mat')['Shepplogan']
+        fdk_data = sio.loadmat(dir + 'data/ccbr_fdk_shepplogan.mat')\
+        ['ccbr_fdk_shepplogan']
+        plt.figure("ori, y=-2.5")
         plt.imshow(original[:, 95, :], vmin=0.98, vmax=1.05, cmap='gray')
         plt.colorbar()
-
-        plt.figure('fdk')
-        plt.imshow(fdk_data[:, 95, :], vmin=123, vmax=126, cmap='gray')
+        # plt.savefig("oriy-25.png")
+ 
+        plt.figure('ccbr, y=-2.5')
+        plt.imshow(fdk_data[:, 95, :], vmin=1.25, vmax=1.35, cmap='gray')
         plt.colorbar()
+        # plt.savefig("ccbry-25.png")
         plt.show()
 
 
-if __name__ == "__main__":
-    ct = FDK("./data/Circular CBCT_flat_panel_detector.prj")
+def main():
+    # c = CCBR()
     a = Analysis()
+
+
+if __name__ == '__main__':
+    main()
